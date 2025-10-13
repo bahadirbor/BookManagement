efc27fed46a3c6ac085d23bbaf52220808d7d8ce
Bahadır Bör <70104032+bahadirbor@users.noreply.github.com>
2025-10-13 12:26:07 +0300

    Created db_context class

---
+++ bookManagement/Data/libraryDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bookManagement.Data;
+
+class libraryDbContext : DbContext {
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+    //    
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(/*YOUR CONNECTION STRING*/);
+    }
+}
