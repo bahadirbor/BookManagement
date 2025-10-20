65438801bea405ad77d3d8e6e5c859b4ff44390d
Bahadır Bör <70104032+bahadirbor@users.noreply.github.com>
2025-10-20 15:26:47 +0300

    Copmleted reservation entity and staff entity configurations

--- bookManagement/Entities/Reservation.cs
+++ bookManagement/Entities/Reservation.cs
@@ -8,13 +8,20 @@ using System.Threading.Tasks;
 
 namespace bookManagement.Entities;
 
-class Reservation
-{
+class Reservation{
+    public int ReservationId { get; set; }
+    public DateTime ReservationDate { get; set; }
+    public int BookId { get; set; }
+    public int UserId { get; set; }
 }
 
 class ReservationConfiguration : IEntityTypeConfiguration<Reservation>
 {
     public void Configure(EntityTypeBuilder<Reservation> builder)
     {
+        builder.HasKey(p => p.ReservationId);
+
+        builder.Property(p => p.ReservationDate)
+               .IsRequired();
     }
 }
--- bookManagement/Entities/Staff.cs
+++ bookManagement/Entities/Staff.cs
@@ -8,14 +8,27 @@ using System.Threading.Tasks;
 
 namespace bookManagement.Entities;
 
-class Staff
-{
+class Staff{
+    public int StaffId { get; set; }
+    public string FirstName { get; set; }
+    public string Surname { get; set; }
+    public string Position { get; set; }
 }
 
-class StaffConfigurator : IEntityTypeConfiguration<Staff>
+class StaffConfiguration : IEntityTypeConfiguration<Staff>
 {
-    public void Configure(EntityTypeBuilder<Staff> builder)
-    {
-       
+    public void Configure(EntityTypeBuilder<Staff> builder){
+        builder.HasKey(s => s.StaffId);
+        
+        builder.Property(b => b.FirstName)
+               .IsRequired()
+               .HasMaxLength(20);
+
+        builder.Property(b => b.Surname)
+               .IsRequired()
+               .HasMaxLength(20);
+
+        builder.Property(b => b.Position)
+            .IsRequired();
     }
 }
