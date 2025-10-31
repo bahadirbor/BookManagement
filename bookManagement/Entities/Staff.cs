0c298c61f1787ba646819b8d02a476c6a43f5ffd
Bahadır Bör <70104032+bahadirbor@users.noreply.github.com>
2025-10-31 12:08:56 +0300

    Data Transfer Object Class and Operations Class implemented

--- bookManagement/Entities/Staff.cs
+++ bookManagement/Entities/Staff.cs
@@ -1,13 +1,9 @@
 ﻿using bookManagement.Data;
-using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace bookManagement.Entities;
 
@@ -39,21 +35,40 @@ class StaffDto{
     public string Surname { get; set; }
 
     [Required]
-    [StringLength(20)]
+    [MaxLength(16)]
     public int Password { get; set; }
 }
 
 class StaffOperations{
     //Staff operations such as Add, Update, Remove
-    private readonly libraryDbContext _context;
+    private readonly LibraryDbContext _context;
 
     public StaffOperations() {
-        _context = new libraryDbContext();
+        _context = new LibraryDbContext();
     }
 
-    public async Task AddStaff(StaffDto dto){
+    public async Task AddStaffAsync(StaffDto dto){
+        var admin = await _context.People.FindAsync(1);
 
         string username = (dto.FirstName[0] + dto.Surname).ToLower();
+
+        bool usernameExists = await _context.Staffs
+            .AnyAsync(s => s.Username == username);
+
+
+        if(usernameExists == false && admin != null)
+        {
+            Console.Write("\nEnter the administrator password to confirm:");
+            int adminPassword = int.Parse(Console.ReadLine());
+            if(admin.Password != adminPassword){
+                Console.WriteLine("Administrator password is incorrect. Staff addition aborted.");
+                return;
+            }
+        }
+        else{
+            Console.WriteLine("Username already exists. Staff addition aborted.");
+            return;
+        }
 
         Staff staff = new Staff() {
             FirstName = dto.FirstName,
@@ -68,21 +83,101 @@ class StaffOperations{
         Console.WriteLine("Staff added successfully.");
     }
 
-    public void UpdateStaff(StaffDto dto){
-        //
+    public async Task UpdateStaffAsync() {
+        Console.Write("Enter the username of the staff to update:");
+        string username = Console.ReadLine();
+
+        if ("admin".Equals(username)){
+            Console.WriteLine("Admin details cannot be updated");
+            return;
+        }
+
+        var staff = await _context.Staffs
+            .Where(s => s.Username == username)
+            .FirstOrDefaultAsync();
+
+        if (staff == null)
+        {
+            Console.WriteLine("Staff with the given username not found.");
+            return;
+        }
+        else {
+            Console.WriteLine("Current Staff Details:");
+            Console.WriteLine($"1. First Name: {staff.FirstName}");
+            Console.WriteLine($"2. Surname: {staff.Surname}");
+            Console.WriteLine($"3. Username: {staff.Username}");
+            Console.WriteLine("4. Password: ********");
+
+            StaffDto dto = new StaffDto();
+
+            Console.Write("\nEnter the number corresponding to the field you want to update: ");
+            int choice = int.Parse(Console.ReadLine());
+
+            Console.Write("\nEnter the admin password for confirmation: ");
+            int password = int.Parse(Console.ReadLine());
+
+            var admin = await _context.People.FindAsync(1);
+
+            if (admin.Password == password)
+            {
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Enter new First Name: ");
+                        dto.FirstName = Console.ReadLine();
+
+                        staff.FirstName = dto.FirstName;
+                        await _context.SaveChangesAsync();
+                        Console.WriteLine("Member name updated successfully.");
+                        break;
+                    case 2:
+                        Console.Write("Enter new Surname: ");
+                        dto.Surname = Console.ReadLine();
+
+                        staff.Surname = dto.Surname;
+                        await _context.SaveChangesAsync();
+                        Console.WriteLine("Member surname updated successfully.");
+                        break;
+                    case 3:
+                        Console.WriteLine("Username cannot be updated.");
+                        break;
+                    case 4:
+                        Console.Write("Enter new Password (max 16 numbers): ");
+                        dto.Password = int.Parse(Console.ReadLine());
+
+                        staff.Password = dto.Password;
+                        await _context.SaveChangesAsync();
+                        Console.WriteLine("Member password updated successfully.");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Update aborted.");
+                        return;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Incorrect password. Update aborted.");
+            }
+        } 
+
     }
 
-    public async Task RemoveStaffWithUsername() {
+    public async Task RemoveStaffWithUsernameAysnc() {
         // Remove staff based on username
         Console.Write("Enter the username of the staff to remove:");
         string username = Console.ReadLine();
+
+        if ("admin".Equals(username)){
+            Console.WriteLine("Admin cannot be deleted");
+            return;
+        }
 
         Console.Write("\nEnter the administrator password to confirm:");
         int adminPassword = int.Parse(Console.ReadLine());
 
         var admin = await _context.People.FindAsync(1);
 
-        if (admin.Password != adminPassword && adminPassword != null)
+        if (admin.Password == adminPassword && adminPassword != null)
         {
             var deletedStaff = await _context.Staffs
                 .Where(s => s.Username == username)
