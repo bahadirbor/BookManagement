7e775113ed724ab3d4663e72ff8899b0c1879a0f
Bahadır Bör <70104032+bahadirbor@users.noreply.github.com>
2025-11-06 11:28:44 +0300

    Implement reservation operations

--- bookManagement/Entities/Reservation.cs
+++ bookManagement/Entities/Reservation.cs
@@ -1,10 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using bookManagement.Data;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace bookManagement.Entities;
 
@@ -13,6 +10,8 @@ class Reservation{
     public DateTime ReservationDate { get; set; }
     public int BookId { get; set; }
     public int UserId { get; set; }
+
+    //Navigation properties
     public Book Book { get; set; }
     public Member User { get; set; }
 }
@@ -26,4 +25,92 @@ class ReservationConfiguration : IEntityTypeConfiguration<Reservation>
         builder.Property(p => p.ReservationDate)
                .IsRequired();
     }
+}
+
+class ReservationDto{
+    public DateTime ReservationDate { get; set; }
+
+    public string BookName { get; set; }
+}
+
+class ReservationOperations{
+    private readonly LibraryDbContext _context;
+    public ReservationOperations()
+    {
+        _context = new LibraryDbContext();
+    }
+    
+    public async Task ShowMemberReservationsAsync(Member member){
+        var reservations = await _context.Reservations
+            .Include(r => r.Book)
+            .Include(r => r.User)
+            .Where(r => r.UserId == member.PersonId)
+            .ToListAsync();
+
+        foreach (var reservation in reservations){
+            Console.WriteLine($"Reservation ID: {reservation.ReservationId}, Date: {reservation.ReservationDate}, Book: {reservation.Book.BookName}, User: {reservation.User.FirstName}");
+        }
+    }
+
+    public async Task ShowAllReservationsAsync(){
+        var reservations = await _context.Reservations
+            .Include(r => r.Book)
+            .Include(r => r.User)
+            .ToListAsync();
+        foreach (var reservation in reservations){
+            Console.WriteLine($"Reservation ID: {reservation.ReservationId}, Date: {reservation.ReservationDate}, Book: {reservation.Book.BookName}, User: {reservation.User.FirstName}");
+        }
+    }
+
+    public async Task AddReservationAsync(ReservationDto reservationDto, Member member){
+        var book = await _context.Books.FirstOrDefaultAsync(d => d.BookName == reservationDto.BookName);
+
+        if (book == null){
+            throw new Exception("Book not found");
+        }
+        
+        var reservation = new Reservation
+        {
+            ReservationDate = DateTime.Now,
+            BookId = book.BookId,
+            UserId = member.PersonId
+        };
+
+        await _context.Reservations.AddAsync(reservation);
+        await _context.SaveChangesAsync();
+        
+    }
+    
+    public async Task UpdateReservationAsync(int reservationId, ReservationDto reservationDto){
+        var reservation = await _context.Reservations.FindAsync(reservationId);
+        
+        if (reservation == null){
+            throw new Exception("Reservation not found");
+        }
+        else{
+            var book = await _context.Books.FirstOrDefaultAsync(d => d.BookName == reservationDto.BookName);
+
+            if (book == null){
+                throw new Exception("Book not found");
+            }
+
+            reservation.ReservationDate = DateTime.Now;
+            reservation.BookId = book.BookId;
+            _context.Reservations.Update(reservation);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public async Task DeleteReservationAsync(int reservationId){
+        var reservation = await _context.Reservations.FindAsync(reservationId);
+        
+        if (reservation == null){
+            throw new Exception("Reservation not found");
+        }
+        else{
+            _context.Reservations.Remove(reservation);
+            await _context.SaveChangesAsync();
+        }
+    }
+
 }
